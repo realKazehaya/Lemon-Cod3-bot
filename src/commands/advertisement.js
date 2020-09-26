f6a058768a92445c8c4ab3d15c1cf519736cc10e
Pablo Sabater <pabloosabaterr@gmail.com>
2020-09-26 20:29:40 +0200

    edited adverts command

--- src/commands/advertisement.js
+++ src/commands/advertisement.js
@@ -9,27 +9,11 @@ const advertisments = async (msg, args) => {
     .setColor(config.bot.color.primary);
 
   if (msg.member.hasPermission("ADMINISTRATOR")) {
-    let aChannel = msg.guild.channels.cache.find(
-      (x) => x.id === config.channels.advertisments
-    ); //where will go the suggestion
-
     if (!args || args.length === 0) {
       msg.reply("por favor escribe lo que desees sugerir despues del comando");
-    } else if (!aChannel) {
-      msg.reply("no se puede encontrar el canal para colocar los anuncios");
     }
     //we say to the user that his message was suscefully sended
-    msg.channel.send(`el anuncio esta en <#${config.channels.advertisments}>`);
-    msg
-      .delete({ timeout: 1000 }) //we remove the command message
-      .then((msg) =>
-        console.log(
-          `Deleted message from ${msg.author.username} after 1 seconds`
-        )
-      )
-      .catch(console.error);
-
-    aChannel.send(sEmbed);
+    msg.channel.send(sEmbed);
   } else {
     msg.reply("no tienes permisos o algo salio mal");
   }
