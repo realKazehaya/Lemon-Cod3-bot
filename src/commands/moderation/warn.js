409154fc86c99d6eb63afacfdd2a61456277bbf5
Pablo Sabater <pabloosabaterr@gmail.com>
2020-09-26 19:24:33 +0200

    fix warning bug

--- src/commands/moderation/warn.js
+++ src/commands/moderation/warn.js
@@ -2,18 +2,19 @@ const { MessageEmbed } = require("discord.js");
 const config = require("../../config");
 
 const warnCommand = async (msg) => {
+  const user = msg.mentions.users.first();
   const wEmbed = new MessageEmbed()
     .setTitle("Warn")
     .setDescription(`el usuario ${user} ah sido avisado con un warning`);
-
-  const user = msg.mentions.users.first();
-  if (msg.member?.roles.cache.has(config.roles.admin)) {
+  if (msg.member.hasPermission("ADMINISTRATOR")) {
+    console.log("has perms");
     if (user) {
       const member = msg.guild?.member(user);
       if (member) {
         if (msg.member?.roles.cache.has(config.roles.warning)) {
           msg.reply(` ${user} ya tiene warning`);
         } else {
+          console.log("warning");
           member.roles.add(config.roles.warning).then(() => {
             msg.channel.send(wEmbed);
           });
