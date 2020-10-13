5e03d0ccd688726ad6e09197f6e9650f8eee4942
realKazehaya <68473169+realKazehaya@users.noreply.github.com>
2020-10-13 20:55:35 +0200

    Update say.js

--- src/commands/say.js
+++ src/commands/say.js
@@ -1,1 +1,9 @@
-//code
+const { MessageEmbed } = require("discord.js");
+const config = require("../config");
+
+const sayCommand = async (msg, args) => {
+  
+if(!args) return message.channel.send(`debe escribir un mensaje a enviar.`);
+message.channel.send(args.join(" "));
+  
+exports.sayCommand = sayCommand
