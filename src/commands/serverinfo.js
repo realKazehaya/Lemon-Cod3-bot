d60592b93af8768a2044fe65f1d8eee0b2a44129
realKazehaya <68473169+realKazehaya@users.noreply.github.com>
2020-10-13 20:51:01 +0200

    Create serverinfo.js

---
+++ src/commands/serverinfo.js
@@ -0,0 +1,14 @@
+var server = message.guild;
+  
+const embed = new Discord.MessageEmbed()
+.setThumbnail(server.iconURL())
+.setAuthor(server.name, server.iconURL())
+.addField('ID', server.id, true)
+.addField('Region', server.region, true)
+.addField('Creado el', server.joinedAt.toDateString(), true)
+.addField('Dueño del Servidor', server.owner.user.tag+' ('+server.owner.user.id +')', true)
+.addField('Miembros', server.memberCount, true)
+.addField('Roles', server.roles.size, true)
+.setColor(0x66b3ff)
+    
+message.channel.send(embed);
